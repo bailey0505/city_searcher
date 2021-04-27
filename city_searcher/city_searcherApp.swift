2704ddd4d8b3b53bdbd5ce1b46d22f7e06ce4b6b
Bailey Rotellini <baileyrotellini1998@gmail.com>
2021-04-27 07:18:32 -0400

    Initial Commit

---
+++ city_searcher/city_searcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  city_searcherApp.swift
+//  city_searcher
+//
+//  Created by Bailey Rotellini on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct city_searcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
